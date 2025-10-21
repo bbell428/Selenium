d5c0637ee927c488c8d3173a5b904b22b1c3ef7b
Bbell <428bbell@gmail.com>
2025-10-21 22:28:57 +0900

    주석 추가

--- Input_Click_Screenshot.py
+++ Input_Click_Screenshot.py
@@ -8,7 +8,7 @@ import time
 # 화면이 꺼지는 현상을 방지
 Options = Options()
 Options.add_argument("--start-maximized") # 화면을 꽉 차게 띄우기
-Options.add_experimental_option("detach", True)
+Options.add_experimental_option("detach", True) # 브라우저가 자동으로 닫히지 않게 하는 옵션
 # ----------------------------
 
 driver = webdriver.Chrome(options=Options)
@@ -35,5 +35,5 @@ time.sleep(2)
 driver.find_element(By.ID, "query").send_keys(Keys.ENTER) # 엔터키를 작동시킴
 time.sleep(5)
 
-driver.save_screenshot("naver_테스트.png")
-driver.quit() # 창 닫기
+# driver.save_screenshot("naver_테스트.png")
+# driver.quit() # 창 닫기
