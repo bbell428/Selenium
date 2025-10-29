7b3b736951d41b0eac8cba447a90e95832c0c08e
Bbell <428bbell@gmail.com>
2025-10-29 16:47:38 +0900

    Headless

--- Headless/headless.py
+++ Headless/headless.py
@@ -10,10 +10,10 @@ options.add_argument("--window-size=1920,1080")
 options.add_argument("--start-maximized")  # 화면을 꽉 차게 띄우기
 # ----------------------------
 
-browser = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options)
 
 url = "https://www.naver.com/"
-browser.get(url)
+driver.get(url)
 
 time.sleep(3)
-browser.get_screenshot_as_file("naver.png")
+driver.get_screenshot_as_file("naver.png")
--- Headless/headless_chrome_useragent.py
+++ Headless/headless_chrome_useragent.py
@@ -7,14 +7,14 @@ options.add_argument("--headless=new")
 options.add_argument("--window-size=1920,1080")
 options.add_argument("User Agent")
 
-browser = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options)
 
-url = "https://www.whatismybrowser.com/detect/what-is-my-user-agent"
-browser.get(url)
+url = "https://www.whatismydriver.com/detect/what-is-my-user-agent"
+driver.get(url)
 
 # Mozilla/5.0 (Windows NT 10.0; Win64; x64) 
 # AppleWebKit/537.36 (KHTML, like Gecko) 
 # Chrome/84.0.4147.89 Safari/537.36
-detected_value = browser.find_element_by_id("")
+detected_value = driver.find_element_by_id("")
 print(detected_value.text)
-browser.quit()
+driver.quit()
